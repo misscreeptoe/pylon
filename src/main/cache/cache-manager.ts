a37494dff4411eaed1d8fb7407fb3ca66b56375b
NathanosDev <contact@nathanos.dev>
2023-03-20 03:30:28 +0100

    feat: add metadata cache

---
+++ src/main/cache/cache-manager.ts
@@ -0,0 +1,98 @@
+import { app } from 'electron';
+import { writeFile, readFile, mkdir } from 'node:fs/promises';
+
+interface Cache<T> {
+  version: number;
+  entries: {
+    [key: string]: {
+      expiry: number;
+      data: T;
+    };
+  };
+}
+
+export class CacheManager<T> {
+  private cache: Cache<T>;
+  private readonly cacheDir: string;
+  private readonly cacheFile: string;
+
+  private constructor(cacheName: string, private readonly version: number) {
+    this.cacheDir = `${app.getPath('userData')}/cache`;
+    this.cacheFile = `${this.cacheDir}/${cacheName}.json`;
+  }
+
+  public static async create<T>(
+    cacheName: string,
+    version: number,
+  ): Promise<CacheManager<T>> {
+    const cacheManager = new CacheManager<T>(cacheName, version);
+
+    await cacheManager.init();
+
+    return cacheManager;
+  }
+
+  public async insert(key: string, expiry: number, data: T): Promise<void> {
+    this.cache.entries[key] = {
+      expiry,
+      data,
+    };
+
+    await this.writeCacheToFile();
+  }
+
+  public get(key: string): T | null {
+    const currentTimestamp = Date.now();
+    const entry = this.cache.entries[key];
+
+    // if the cache entry is missing
+    if (!entry) {
+      return null;
+    }
+
+    // if any of the cache entry's properties are missing (it is corrupt),
+    // or the expiry date is in the past
+    if (!entry.expiry || !entry.data || entry.expiry < currentTimestamp) {
+      this.remove(key);
+      return null;
+    }
+
+    return entry.data;
+  }
+
+  public async remove(key: string): Promise<void> {
+    delete this.cache.entries[key];
+
+    await this.writeCacheToFile();
+  }
+
+  private async init(): Promise<void> {
+    try {
+      await this.hydrateCache();
+    } catch {
+      await this.initCache();
+    }
+  }
+
+  private async hydrateCache(): Promise<void> {
+    const cacheJson = await readFile(this.cacheFile, { encoding: 'utf-8' });
+
+    this.cache = JSON.parse(cacheJson);
+  }
+
+  private async initCache(): Promise<void> {
+    this.cache = {
+      version: this.version,
+      entries: {},
+    };
+
+    await mkdir(this.cacheDir, { recursive: true });
+    await this.writeCacheToFile();
+  }
+
+  private async writeCacheToFile(): Promise<void> {
+    const cacheJson = JSON.stringify(this.cache);
+
+    await writeFile(this.cacheFile, cacheJson, { encoding: 'utf-8' });
+  }
+}
---
+++ src/main/protocols/ic-metadata/metadata-cache.ts
@@ -0,0 +1,58 @@
+import { CacheManager } from '../../cache';
+import { EnrichedMetadata } from './enrich-metadata';
+
+const IC_METADATA_CACHE_TTL = 10 * 60 * 60 * 1_000; // hours * minutes per hour * seconds per minute * milliseconds per second
+
+interface CachedMetadata extends Omit<EnrichedMetadata, 'icon'> {
+  icon: string;
+}
+
+let cacheManager: CacheManager<CachedMetadata> | null = null;
+let cacheManagerPromise: Promise<CacheManager<CachedMetadata>> | null = null;
+
+async function getCacheManager(): Promise<CacheManager<CachedMetadata>> {
+  if (cacheManager) {
+    return cacheManager;
+  }
+
+  if (cacheManagerPromise) {
+    return await cacheManagerPromise;
+  }
+
+  cacheManagerPromise = CacheManager.create('ic-metadata', 1);
+
+  cacheManager = await cacheManagerPromise;
+  return cacheManager;
+}
+
+export async function getMetadataCacheEntry(
+  canisterId: string,
+): Promise<EnrichedMetadata | null> {
+  const cache = await getCacheManager();
+
+  const cacheEntry = cache.get(canisterId);
+
+  if (!cacheEntry) {
+    return null;
+  }
+
+  return {
+    ...cacheEntry,
+    icon: Buffer.from(cacheEntry.icon, 'base64'),
+  };
+}
+
+export async function insertMetadataCacheEntry(
+  canisterId: string,
+  metadata: EnrichedMetadata,
+) {
+  const cache = await getCacheManager();
+
+  const currentTimestamp = Date.now();
+  const expiry = currentTimestamp + IC_METADATA_CACHE_TTL;
+
+  await cache.insert(canisterId, expiry, {
+    ...metadata,
+    icon: metadata.icon.toString('base64'),
+  });
+}
