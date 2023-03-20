fcb8cfecf2377c83391fcc70709cfbfe534c7abb
NathanosDev <contact@nathanos.dev>
2023-03-20 23:55:44 +0100

    feat: add support for forwarding api requests

--- src/main/protocols/ic/ic-request-handler.ts
+++ src/main/protocols/ic/ic-request-handler.ts
@@ -3,6 +3,7 @@ import {
   makeIcHttpRequest,
   tryParseIcHttpRequestUrl,
 } from '../../http-gateway';
+import { forwardIcApiRequest, isIcApiRequest } from './api-request';
 import { forwardStandardHttpRequest } from './forward-standard-http-request';
 
 export async function icRequestHandler(
@@ -12,14 +13,22 @@ export async function icRequestHandler(
     const parsedIcHttpRequestUrl = tryParseIcHttpRequestUrl(request.url);
 
     if (!parsedIcHttpRequestUrl) {
+      if (isIcApiRequest(request.url)) {
+        return await forwardIcApiRequest(request);
+      }
+
       return await forwardStandardHttpRequest(request);
     }
 
     return await makeIcHttpRequest(parsedIcHttpRequestUrl.canisterId, request);
-  } catch {
-    // [TODO]: Handle error
+  } catch (error) {
+    console.error('Encountered a fatal error');
+    console.error('Original request', request);
+    console.error('Error', error);
+
     return {
-      data: '',
+      statusCode: 500,
+      data: String(error),
     };
   }
 }
