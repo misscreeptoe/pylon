3f557f3616eeaa76e682902df1b933ec89960eff
NathanosDev <contact@nathanos.dev>
2023-04-07 00:29:05 +0200

    fix: issue with preload.js file not working on packaged apps

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -1,8 +1,6 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
 import type { Configuration } from 'webpack';
 
 import { rules } from './webpack.rules';
-import { plugins } from './webpack.plugins';
 
 export const rendererConfig: Configuration = {
   module: {
@@ -14,7 +12,6 @@ export const rendererConfig: Configuration = {
       },
     ],
   },
-  plugins: [...plugins, new HtmlWebpackPlugin()],
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
     alias: {
